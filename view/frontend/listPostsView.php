33e7236748e5c53de6f51b108350a7b9d243b51f
Wasamii <erwann.wansch@hotmail.fr>
2020-11-21 11:25:17 +0100

    Change direction db post

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -12,13 +12,13 @@ while ($data = $posts->fetch())
     <div class="news">
         <h3>
             <?= htmlspecialchars($data['title']) ?>
-            <em>le <?= $data['creation_date_fr'] ?></em>
+            <em>le <?= $data['date_post_fr'] ?></em>
         </h3>
         
         <p>
-            <?= nl2br(htmlspecialchars($data['content'])) ?>
+            <?= nl2br(htmlspecialchars($data['text_post'])) ?>
             <br />
-            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
+            <em><a href="index.php?action=post&amp;id=<?= $data['id_post'] ?>">Commentaires</a></em>
         </p>
     </div>
 <?php
