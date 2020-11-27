9aa5aadcf3f108f1e70bc7874cf13dd06a26fe48
Wasamii <erwann.wansch@hotmail.fr>
2020-11-27 12:28:50 +0100

    change sigin to siginView separate page

--- view/frontend/menu.php
+++ view/frontend/menu.php
@@ -1,19 +1,5 @@
 
 <form method="formsend">
-        <div>
-        <!--Temporaire coté inscription cela sera une autre page 
-        input sera remplacé par une balise <a></a> qui redirigera dessus -->
-        <p>Inscription</p>
-        <label for="pseudo">Pseudo: </label>
-        <input type="name" name="pseudo"></br>
-        <label for="email">Email: </label>
-        <input type="email" name="email"></br>
-        <label for="password">Mot de Passe: </label>
-        <input type="password" name="password"></br>
-        <label for="cpassword">Confirmer mot de Passe: </label>
-        <input type="password" name="cpassword"></br>        
-        <input type="submit" value="inscription" name="sigin">
-        </div>
         </br>
         <div>
         <p>Connexion</p>
@@ -22,6 +8,7 @@
         <label for="lpassword">Mot de passe: </label>
         <input type="lpassword" name="lpassword"></br>
         <input type="checkbox" name="cnxauto" value="connexion auto"><p id="connectauto">Connexion auto</p></br>
-        <input type="submit" value="connexion" name="login">
+        <input type="submit" value="connexion" name="login"></br>
+        <a href="view/frontend/siginView.php">Inscription</a>
         </div>
 </form>
---
+++ view/frontend/siginView.php
@@ -0,0 +1,16 @@
+<form method="formsend">
+    <div>
+        <!--Temporaire coté inscription cela sera une autre page 
+        input sera remplacé par une balise <a></a> qui redirigera dessus -->
+        <p>Inscription</p>
+        <label for="pseudo">Pseudo: </label>
+        <input type="name" name="pseudo" placeholder="pseudo"></br>
+        <label for="email">Email: </label>
+        <input type="email" name="email" placeholder="email"></br>
+        <label for="password">Mot de Passe: </label>
+        <input type="password" name="password" placeholder="password"></br>
+        <label for="cpassword">Confirmer mot de Passe: </label>
+        <input type="password" name="cpassword"></br>        
+        <input type="submit" value="inscription" name="sigin">
+        </div>
+</form>
