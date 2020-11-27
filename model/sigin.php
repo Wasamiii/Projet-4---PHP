63d73f2b2baff7264939dd72bd06b597040afc69
Wasamii <erwann.wansch@hotmail.fr>
2020-11-27 12:28:12 +0100

    Add members_date=now

--- model/sigin.php
+++ model/sigin.php
@@ -3,7 +3,7 @@ namespace Wamp\www\model;
 require_once('model/Manager.php');
 
 //!cela ne fait rien c'est juste pour avoir la forme mais mettre login et sigin dans le même fichier
-// * * L'inscription sera sur une autre pas et un bouton redirigera dessus
+// * * L'inscription sera sur une autre page et un bouton redirigera dessus
 class Singin extends Manager 
 {
     
@@ -19,8 +19,7 @@ class Singin extends Manager
                 
                 if($password == $cpassword){
                     $pass_hache = password_hash($password, PASSWORD_DEFAULT);
-
-                            $req = $db->prepare('INSERT INTO members(pseudo, password, email) VALUES(:pseudo, :password, :email)');
+                            $req = $db->prepare('INSERT INTO members(pseudo, password, email,members_date) VALUES(:pseudo, :password, :email, NOW())');
                             $req->execute(array(
                                 'pseudo' => $pseudo,
                                 'email' => $email,
