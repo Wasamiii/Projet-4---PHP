4b9fc944d8e1bddc130f2ac0ba15c4c8da73b531
Wasamii <erwann.wansch@hotmail.fr>
2020-12-08 12:23:45 +0100

    Change label for cnxauto

--- view/frontend/menu.php
+++ view/frontend/menu.php
@@ -5,7 +5,7 @@
         <input type="lemail" name="lemail" required></br>
         <label for="lpassword">Mot de passe: </label>
         <input type="lpassword" name="lpassword" required></br>
-        <label for="checkbox"><input type="checkbox" name="cnxauto" value="connexion auto">Connexion Auto</label></br>
+        <label for="cnxauto"><input type="checkbox" name="cnxauto" value="connexion auto">Connexion Auto</label></br>
         <input type="submit" value="connexion" name="signin"></br>
         <a href="index.php?action=members">Inscription</a>
         </div>
