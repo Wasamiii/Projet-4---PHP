c6251641346138fcf076b71fad58e5db6547f98b
Wasamii <erwann.wansch@hotmail.fr>
2020-12-08 12:39:07 +0100

    change email to pseudo and password instead of lpassword

--- view/frontend/menu.php
+++ view/frontend/menu.php
@@ -1,10 +1,10 @@
 <form action="index.php?action=getlog" method="post">
         <div>
         <p>Connexion</p>
-        <label for="lemail">Email: </label>
-        <input type="lemail" name="lemail" required></br>
-        <label for="lpassword">Mot de passe: </label>
-        <input type="lpassword" name="lpassword" required></br>
+        <label for="pseudo">Pseudo: </label>
+        <input type="name" name="pseudo" required></br>
+        <label for="password">Mot de passe: </label>
+        <input type="password" name="password" required></br>
         <label for="cnxauto"><input type="checkbox" name="cnxauto" value="connexion auto">Connexion Auto</label></br>
         <input type="submit" value="connexion" name="signin"></br>
         <a href="index.php?action=members">Inscription</a>
