4bca19ec4d12688f61fa3ca9328f6797c872cf0f
Wasamii <erwann.wansch@hotmail.fr>
2020-12-09 11:35:55 +0100

    Add redirection auto disconnect action with index.php

--- index.php
+++ index.php
@@ -42,11 +42,11 @@ try { // On essaie de faire des choses
                 verifyLogin();
         }elseif($_GET['action'] == 'disconnect'){
             session_destroy();
+            header('Location: index.php');
         }
-    }
-    else {
-        listPosts();
-    }
+    }else{
+    listPosts();
+    }   
 }
 catch(Exception $e) { // S'il y a eu une erreur, alors...
     echo 'Erreur : ' . $e->getMessage();
