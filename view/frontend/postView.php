55ed83ee1d9d56403eafddf3020d8a9a520f4c57
Wasamii <erwann.wansch@hotmail.fr>
2020-12-29 14:00:50 +0100

    Change report to icon flag

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -42,7 +42,7 @@
             <p><strong><?= htmlspecialchars($comment['pseudo']) ?></strong> le <?= $comment['comment_date_fr'] ?>
             <?php
             if(isset($_SESSION['id'])){?>
-            (<a href="" id="report">Signaler</a>)</p>
+            <a href="" id="report"><i class="far fa-flag">Signaler</i></a></p>
         <?php
         }
         ?>
