b01761f5c7acaef8e41fb8167052137e848f6db8
Wasamii <erwann.wansch@hotmail.fr>
2021-01-02 11:57:00 +0100

    Add TinyMCE just sperarate to comment textarea

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -5,9 +5,24 @@
         <title><?= $title ?></title>
         <link rel="stylesheet" href="public/CSS/style.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+        <script src="https://cdn.tiny.cloud/1/n11767hjwn9rpu0tdqxd0ul2yyve71z7k5rakdrdv0ldvjmc/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     </head> 
     <body>
         <?php require("menu.php")?>
+        <textarea id="tinymce">
+    Welcome to TinyMCE!
+  </textarea>
+  <script>
+    tinymce.init({
+      selector: 'textarea',
+      menubar: false,
+      plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+      toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+      toolbar_mode: 'floating',
+      tinycomments_mode: 'embedded',
+      tinycomments_author: 'Author name',
+   });
+  </script>
         <?= $content ?>
     </body>
     <footer>
