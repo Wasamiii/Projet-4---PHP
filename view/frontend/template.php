886fb6a7b7441e86d4a2e01bf7f37568d83b51aa
Wasamii <erwann.wansch@hotmail.fr>
2021-01-15 12:41:59 +0100

    change to admin.php

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -4,28 +4,10 @@
         <meta charset="utf-8" />
         <title><?= $title ?></title>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-        <script src="https://cdn.tiny.cloud/1/n11767hjwn9rpu0tdqxd0ul2yyve71z7k5rakdrdv0ldvjmc/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
         <link rel="stylesheet" href="public/CSS/style.css">
     </head> 
     <body>
         <?php require("menu.php")?>
-        
-        <?php if($_SESSION['admin'] == "1"){ ?>
-        <textarea id="tinymce" placeholder="Welcome to TinyMCE!"></textarea>
-  <script>
-    tinymce.init({
-      selector: '#tinymce',
-      menubar: false,
-      plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
-      toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | preview save print | insertfile image media link anchor | ltr rtl',
-      toolbar_mode: 'floating',
-      tinycomments_mode: 'embedded',
-      tinycomments_author: 'Author name',
-   });
-  </script>
-<?php
-}
-  ?>
         <?= $content ?>
     </body>
     <footer>
