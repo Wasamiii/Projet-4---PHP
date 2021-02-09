64416847fd0288010b16f2c00692d96531919f1f
Wasamii <erwann.wansch@hotmail.fr>
2021-02-09 15:06:10 +0100

    sapce bug fix

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -6,12 +6,12 @@
         <title><?= $title ?></title>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
         <link rel="stylesheet" href="public/CSS/style.css">
-    </head> 
+    </head>
     <body>
         <?php require_once("menu.php")?>
-        <?= $content ?>
+        <?=$content?>
     </body>
     <footer>
-    <?php require_once("footer.php")?>  
+    <?php require_once("footer.php")?>
     </footer>
 </html>
