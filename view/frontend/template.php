86cdda276657cff48c5ba9d3d6da4dacb1c9bb31
Wasamii <erwann.wansch@hotmail.fr>
2021-02-11 13:39:47 +0100

    Add icon on title bar

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="public/img/icone-d-ecrivain.png" type="image/png">
         <title><?= $title ?></title>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
         <link rel="stylesheet" href="public/CSS/style.css">
