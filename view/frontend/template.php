2b2082eef626f8b990a18981f3ab0a9a8b650b12
Wasamii <erwann.wansch@hotmail.fr>
2021-02-15 17:02:21 +0100

    change logo and add img beautiful site

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="public/img/icone-d-ecrivain.png" type="image/png">
+        <link rel="shortcut icon" href="public/img/1138px-P_author.svg_1.png" type="image/png">
         <title><?= $title ?></title>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
         <link rel="stylesheet" href="public/CSS/style.css">
@@ -12,6 +12,7 @@
         <?php require_once("menu.php")?>
     </header>
     <body>
+        <img src="public/img/Forests_Mountains_Rivers_USA_Alaska_568452_1920x1080.png" id="alaska-img">
         <?=$content?>
     </body>
     <footer>
