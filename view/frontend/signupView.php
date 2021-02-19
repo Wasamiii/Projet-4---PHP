1f24cf1e7018f121461c1795e8e6195a96dcebc3
Wasamii <erwann.wansch@hotmail.fr>
2021-02-19 15:37:28 +0100

    add class and id for style.css

--- view/frontend/signupView.php
+++ view/frontend/signupView.php
@@ -1,17 +1,20 @@
-<form action="index.php?action=validSignup" method="post">
-    <div>
-        <p>Inscription</p>
-        <label for="pseudo">Pseudo: </label>
-        <input type="name" name="pseudo" required></br>
-        <label for="email">Email: </label>
-        <input type="email" name="email" required></br>
-        <label for="password">Mot de Passe: </label>
-        <input type="password" name="password" placeholder="password"required></br>
-        <label for="cpassword">Confirmer mot de Passe: </label>
-        <input type="password" name="cpassword"required></br>        
-        <input type="submit" name="inscription" value="s'inscrire">
+<?php $title = 'Mon blog'; ?>
+<?php ob_start(); ?>
+<form action="index.php?action=validSignup" method="post"   id="Signupview">
+    <div id="contentSignup">
+        <p id="textSinup">Inscription</p>
+        <label for="pseudo" class="signupLabel">Pseudo: </label>
+        <input type="name" name="pseudo"  class="inputSignup"  required></br>
+        <label for="email" class="signupLabel">Email: </label>
+        <input type="email" name="email" class="inputSignup" required></br>
+        <label for="password" class="signupLabel">Mot de Passe: </label>
+        <input type="password" name="password" class="inputSignup" required></br>
+        <label for="cpassword" class="signupLabel">Confirmer mot de Passe: </label>
+        <input type="password" name="cpassword" class="inputSignup" required></br>        
+        <input type="submit" name="inscription" value="s'inscrire" id="signupButton">
         </div>
 </form>
-<div>
-<p><a href="index.php">Retour à la page principale</a>
-</div>
+<?php $content = ob_get_clean(); ?>
+
+
+<?php require_once('view/frontend/template.php'); ?>
